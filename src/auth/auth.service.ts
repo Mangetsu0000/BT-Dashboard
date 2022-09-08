8bbcd81d4b6dfe5484f4355ff9bc631ad0345630
Mouelhi Mohamed Fakhreddine <m.mouelhi0603@myu.universitecentrale.tn>
2022-09-08 12:39:40 +0100

    updated authDto

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -17,6 +17,8 @@ export class AuthService {
         data: {
           email: dto.email,
           hash,
+          firstName: dto.firstName,
+          lastName: dto.lastName,
         },
       });
 
